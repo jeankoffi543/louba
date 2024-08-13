a34e56cc2c5f0638030bf35a713fb5d86410ee71
jeankoffi543 <jeankoffi543@gmail.com>
2024-08-13 02:14:28 +0000

    bugs - qrcode sur reçu par email, heure sur date rdv, production de document erreur 404

--- resources/views/client/attestationAppointmentPdf.blade.php
+++ resources/views/client/attestationAppointmentPdf.blade.php
@@ -138,7 +138,7 @@
                 {{ \Carbon\Carbon::parse($maDemande->client->date_naissance_client)->format('d-m-Y') }}
             </span>
 
-            A: {{ $maDemande->client->birth_address }}
+            A: {{ $maDemande->birth_address }}
         </p>
 
         <p>
